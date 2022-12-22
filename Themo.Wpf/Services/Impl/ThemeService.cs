3b256b8cd8b83ccc061489a304c6e87d8b51497f
Lukas Pfanner <lukas.pfann@gmail.com>
2022-12-22 21:36:22 +0100

    comments

--- Themo.Wpf/Services/Impl/ThemeService.cs
+++ Themo.Wpf/Services/Impl/ThemeService.cs
@@ -11,9 +11,21 @@ namespace Themo.Wpf.Services.Impl
 {
     public class ThemeService<TEnum> : IThemeService<TEnum> where TEnum : struct
     {
+        #region events
         public event EventHandler<ThemeChangedEventArgs> ThemeChanged;
+        #endregion
 
+        #region fields
         private TEnum? _currentTheme;
+
+        private ResourceDictionary ThemeDictionary
+        {
+            get => Application.Current.Resources.MergedDictionaries[0];
+            set => Application.Current.Resources.MergedDictionaries[0] = value;
+        }
+        #endregion
+
+        #region props
         public TEnum? CurrentTheme
         {
             get
@@ -24,11 +36,9 @@ namespace Themo.Wpf.Services.Impl
             set => _currentTheme = value;
         }
 
-        private ResourceDictionary ThemeDictionary
-        {
-            get => Application.Current.Resources.MergedDictionaries[0];
-            set => Application.Current.Resources.MergedDictionaries[0] = value;
-        }
+        #endregion
+
+        #region methods
 
         private void ChangeTheme(Uri uri, TEnum theme)
         {
@@ -36,6 +46,11 @@ namespace Themo.Wpf.Services.Impl
             ThemeChanged?.Invoke(this, new ThemeChangedEventArgs(theme));
         }
 
+        /// <summary>
+        /// Sets the theme according to the passed in enum value
+        /// Note: It is expected, your themes are located in your project root in a directory called Themes
+        /// </summary>
+        /// <param name="theme">The desired enum value, which has to exactly match your .xaml file</param>
         public void SetTheme(TEnum theme)
         {
             try
@@ -48,11 +63,17 @@ namespace Themo.Wpf.Services.Impl
             }
         }
 
+        /// <summary>
+        /// Checks which resource dictionary is currently loaded
+        /// </summary>
+        /// <returns></returns>
         public TEnum GetCurrentTheme()
         {
             var loadedDictionary = Application.Current.Resources.MergedDictionaries[0];
             string dictionaryContent = loadedDictionary.Source.ToString();
             return Enum.GetValues(typeof(TEnum)).Cast<TEnum>().FirstOrDefault(x => dictionaryContent.EndsWith(x.ToString()));
         }
+
+        #endregion
     }
 }
